9e15d1e3011d9ca2315b819636f22a85c6b1ab10
Piyush <Piyushmevliwal1241@gmail.com>
2026-06-13 12:12:25 +0530

    fix: socket.js safeCb + misc fixes

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = require('./app');
 const { initSocket } = require('./socket');
 const { connectDB } = require('./db');
-
+app.get('/health', (req, res) => res.json({ ok: true }));
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
